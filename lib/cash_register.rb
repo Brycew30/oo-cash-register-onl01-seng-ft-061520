74acb8313b6ca93427e801b155fa66e19db9226a
Bryce Whittington <brycew30@gmail.com>
2020-06-26 18:39:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     @total += price * quantity
-    @items << title * quantity
+    @items << title * quantity.to_i
   end
   
   def apply_discount
